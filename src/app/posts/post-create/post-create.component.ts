c04526f850756f9263f976fa2d2999c95aa7820a
ashwinpaulallen <ashwin23paul@outlook.com>
2021-05-20 00:37:30 +0530

    Posts form and list

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,10 +1,26 @@
+import { formatCurrency } from "@angular/common";
 import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+import { PostService } from "../posts.service";
 
 @Component({
   selector: 'app-post-create',
+  styleUrls: ['./post-create.component.css'],
   templateUrl: './post-create.component.html'
 })
 
 export class PostCreateComponent {
 
+    constructor(public postService: PostService) {}
+
+    onAddPost(fPost: NgForm) {
+      if (fPost.invalid) {
+        return;
+      }
+
+      this.postService.addPosts(fPost.value.title, fPost.value.content);
+      alert ('Post Aadded!!!!');
+      fPost.resetForm();
+    }
 }
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from 'rxjs';
+import { Post } from "../post.model";
+import { PostService } from "../posts.service";
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent implements OnInit, OnDestroy {
+  // posts = [
+  //   {title: 'First Post', content: 'First Post Content'},
+  //   {title: 'Second Post', content: 'Second Post Content'},
+  //   {title: 'Third Post', content: 'Third Post Content'},
+  //   {title: 'Fourth Post', content: 'Fourth Post Content'},
+  //   {title: 'Fifth Post', content: 'Fifth Post Content'}
+  // ];
+  posts: Post[] = [];
+  private postSub: Subscription;
+
+  constructor(public postService: PostService) {}
+
+  ngOnInit() {
+    this.posts = this.postService.getPosts();
+    this.postSub = this.postService.getPostUpdated().subscribe((posts: Post[]) => {
+      this.posts= posts;
+    });
+  }
+
+  ngOnDestroy() {
+    this.postSub.unsubscribe();
+  }
+}
